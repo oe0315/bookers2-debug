6a5d02e81bdbf3143d00ef6a3adbe069bcd0d224
oe0315 <oeoe0315@gmail.com>
2020-04-05 00:01:12 +0900

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,23 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :current_user, only: [:edit, :update, :delete]
 
   def show
   	@book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
+    @books = Book.all
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
-  		@books = Book.all
   		render 'index'
   	end
   end
@@ -42,7 +46,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :index, :edit, :update]
   resources :books
-  root to: 'home#top'
+  root 'home#top'
   get "home/about" => "home#about"
 end
