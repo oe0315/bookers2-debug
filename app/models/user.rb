d3e33525470e12f0118f63c502cbde8eac04da2e
oe0315 <oeoe0315@gmail.com>
2020-04-09 16:24:13 +0900

    [update]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   attachment :profile_image
 
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
